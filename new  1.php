62212dd13d67bfd76e0dd868c4b9b7de654e0c16
dyovanna <dyovanna40@gmail.com>
2016-08-03 12:04:51 -0300

    commit professor

---
+++ new  1.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+	<meta charset"utf-8"/>
+	<title>tudo sobre google glass</title>
+</head>
+<body>
+	<div id ="interface">
+	<hgroup>
+	<h1>google glass<h1>
+	<h2>a revolução do google esta chegando</h2>
+	</hgroup>
+<img src="imagens/contato.png"
+- Home
+- Especificações
+- Fotos
+- Multimídia
+- Fale conosco
+
+Fale Contato
+Formulário de Contato
+por Gustavo Guanabara
+Atualizado em 01/Maio/2013
+
+Identificação do Usuário
+    Nome:
+    Senha:
+    E-mail:
+    Sexo:
+        - Masculino
+        - Feminino
+    Data de Nascimento:
+
+Endereço do Usuário
+    Logradouro:
+    Número:
+    Estado:
+    Cidade:
+
+Mensagem do Usuário
+    Grau de Urgência:
+    Mensagem:
+
+Quero um Google Glass
+    Gostaria de adquirir um Google Glass quando disponível
+    Cor:
+    Quantidade:
+    Preço Total: R$
+
+
+[BOTÃO ENVIAR]
+
+Copyright 2013 - by Gustavo Guanabara
+Facebook | Twitter
+</body>
+	
+</html>
