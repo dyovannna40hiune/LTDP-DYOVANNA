de70ccafcc81e6911616094caeadacb43182279b
dyovanna <dyovanna40@gmail.com>
2016-09-05 08:54:20 -0300

    atualizaçao

--- projeto-feira/salvarinscricao.php
+++ projeto-feira/salvarinscricao.php
@@ -2,9 +2,9 @@
 	
 	
 	$podeinserir=true;
-	$nome = $_POST['camponome'];
-	$codigo=  $_POST['campocodigo'];
-	$turma = $_POST['campoturma'];
+	$nome = $_POST['nome'];
+	$codigo =  $_POST['meucodigo'];
+	$turma = $_POST['turma'];
 	$opcao1 = $_POST['opcao1'];
 	$opcao2 = $_POST['opcao2'];
 	
@@ -22,7 +22,7 @@
 	/*se (nao conexao) entao, ou seja, conexao e falsa*/
 	
 	if (!$conexao) {
-		echo "Não foi possível connectar ao servidor";
+		echo "Não foi possível connectar ao servidor".mysql_errno() .mysql_errno();
 		exit;
 	}else{/*senao*/
 		 "<h1>Conectou</h1>";
@@ -31,82 +31,6 @@
 	/*Selecione o banco de dados ou morra*/
 	$banco = mysql_select_db($nome_banco, $conexao) or die ("Não foi possível conectar ao banco de dados");
 	
-	/*Quantidade de vagas no centro 1*/
-	
-	
-	$comandosql = "SELECT  quantidade_de_vagas FROM tb_centro_interesse WHERE id_centro_interesse = ".$opcao1;
-	$resultado = mysql_query($comandosql);
-	
-	if (mysql_errno()) {
-		$error = "MySQL error ".mysql_errno().": ".mysql_error()."\n<br>Quando executou:<br>\n$comandosql\n<br>";
-		echo $error;
-	}
-	$itembancodados = mysql_fetch_array($resultado);
-	$qtdevagas1 = $itembancodados['quantidade_de_vagas'];
-	
-	
-	$comandosql = "SELECT count(*) as total FROM tb_inscricao WHERE opcao_um=".$opcao1;
-	$resultado = mysql_query($comandosql);
-	$itembancodados = mysql_fetch_assoc($resultado);
-	$qtdeinscritosc1 = $itembancodados['total'];
-	
-	if ($qtdeinscritosc1 >= $qtdevagas1){
-		echo "<h1>Não existem vagas na sua primeira opção, tente novamente</h1>";
-		echo "<a href='inscricao.html'>Clique aqui para voltar</a>";
-		$podeinserir=false;
-	} 
-		
-	/*Quantidade de vagas no centro 2*/
-	$comandosql = "SELECT  quantidade_de_vagas FROM tb_centro_interesse WHERE id_centro_interesse = ".$opcao2;
-	$resultado = mysql_query($comandosql);
-	
-	if (mysql_errno()) {
-		$error = "MySQL error ".mysql_errno().": ".mysql_error()."\n<br>Quando executou:<br>\n$comandosql\n<br>";
-		echo $error;
-	}
-	$itembancodados = mysql_fetch_array($resultado);
-	$qtdevagas2 = $itembancodados['quantidade_de_vagas'];
-	
-	$comandosql = "SELECT count(*) as total FROM tb_inscricao WHERE opcao_dois=".$opcao2;
-	$resultado = mysql_query($comandosql);
-	
-
-	if (mysql_errno()) {
-		$error = "MySQL error ".mysql_errno().": ".mysql_error()."\n<br>Quando executou:<br>\n$comandosql\n<br>";
-		echo $error;
-	}
-	$itembancodados = mysql_fetch_assoc($resultado);
-	$qtdeinscritosc2 = $itembancodados['total'];
-	
-	
-	if ($qtdeinscritosc2 >= $qtdevagas2){
-		echo "<h1>Não existem vagas na sua segunda opção, tente novamente</h1>";
-		echo "<a href='inscricao.html'>Clique aqui para voltar</a>";
-		$podeinserir=false;
-	}
-	
-	/*verifique o codigo do aluno*/
-
-	
-	$comandosql = "SELECT count(*) as total FROM tb_inscricao WHERE matricula=".$codigo;
-	$resultado = mysql_query($comandosql);
-	$itembancodados = mysql_fetch_assoc($resultado);
-	$qtdecodigodoaluno = $itembancodados['total'];
-
-	if ($qtdequantidade>=1 ){
-		echo "<h1>Esse código do aluno ja esta cadastrado, tente novamente</h1>";
-		echo "<a href='inscricao.html'>Clique aqui para voltar</a>";
-		$podeinserir=false;
-	}
-	
-	
-	
-	if($podeinserir){	
-
-		$comandosql = "INSERT INTO tb_inscricao VALUES ('','$codigo', '$nome', '$turma', '$opcao1','$opcao2', '2016-08-24', '07:10:00')";		
-		 $comandosql;
-		
-		$resultado = mysql_query($comandosql);
 		/*Encerra a conexao*/
 		
 		if (mysql_errno()) { 
@@ -116,6 +40,5 @@
 			echo "<h1>Cadastro Realizado com sucesso</h1>";
 			echo "<a href='inscricao.html'>Clique aqui para realizar uma nova inscricao</a>";
 		}
-	}
 	mysql_close();
 ?>
